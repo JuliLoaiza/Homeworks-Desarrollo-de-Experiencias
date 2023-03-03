be14cf9e14953ab6785f2814a03a223097f76518
JuliLoaiza <juliana.loaiza@uao.edu.co>
2023-03-03 18:08:57 -0500

    Challenge 3

---
+++ challenges/src/ComponentApp.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useState } from "react";
+
+const categories = ["first category", "second category"];
+const setCategory = () => { };
+const ComponentApp = () => {
+
+    const [category, setCategory] = useState("");
+
+    const inputChange = (e) => {
+        console.log(e.target.value);
+        setCategory(e.target.value);
+    };
+
+    return (
+        <>
+            <h1>GifExpert</h1>
+            <ol>
+                {categories.map((category, key) => {
+                    return <li key={key}> {category}</li>;
+                })}
+            </ol>
+            <input
+                type="text"
+                onChange={(e) => {
+                    inputChange(e);
+                }}
+            ></input>
+            <button onClick={() => setCategory()}> category </button>
+        </>
+    );
+};
+
+export default ComponentApp;
